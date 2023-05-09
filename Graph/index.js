5ace92a4157b1aae3e1140743f407b4fc26a0b93
Josh Kesler <Josh.Kesler@aristocrat.com>
2023-05-09 00:47:56 -0500

    Add graph directory, html skeleton, canvas

---
+++ Graph/index.js
@@ -0,0 +1,24 @@
+//Function === f(x)= (x^2 + 9x + 20) / (x + 4)
+
+const canvas = document.createElement('canvas'); // document.createElement
+canvas.style.height = '1000px';
+canvas.style.width = '1000px';
+const context = canvas.getContext('2d'); // context -> how we interact with the canvas's draw API
+canvas.height = 1000;
+canvas.width = 1000;
+
+const dpi = window.devicePixelRatio;
+document.body.append(canvas);
+
+context.fillRect(500, 500, 10, 10);
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 10;
+        this.height = 10;
+    }
+    get x() {return this.x};
+    get y() {return this.y};
+}
